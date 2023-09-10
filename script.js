e4010b021ee60a7366d61e8ec5362ef1ebff4df6
Hariesh S R <121657436+HARIESH-SR@users.noreply.github.com>
2023-09-10 15:09:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const navBar = document.querySelector("nav"),
        menuBtns = document.querySelectorAll(".menu-icon"),
        overlay = document.querySelector(".overlay");
 
+      
+
      menuBtns.forEach((menuBtn) => {
        menuBtn.addEventListener("click", () => {
          navBar.classList.toggle("open");
